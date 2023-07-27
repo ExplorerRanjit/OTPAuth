858a22b9beb4f88215bd059d1e3e32598f5ba084
RUNRanjit <88085245+RUNRanjit@users.noreply.github.com>
2023-07-27 21:02:12 +0530

    First Commit

--- OTPAuth/settings.py
+++ OTPAuth/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=jq=r%gnf-8at)oo&+e+fmtq(ez95*2#q9_$jkk2=**qw4zrks'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'OTPAuthApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'OTPAuth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#TWILIO SETTINGS
+ACCOUNT_SID= config('ACCOUNT_SID')
+AUTH_TOKEN= config('AUTH_TOKEN')
+TWILIO_WHATSAPP_NUMBER= config('TWILIO_WHATSAPP_NUMBER')
+TWILIO_PHONE_NUMBER= config('TWILIO_PHONE_NUMBER')
+COUNTRY_CODE= config('COUNTRY_CODE')
+
+
+
